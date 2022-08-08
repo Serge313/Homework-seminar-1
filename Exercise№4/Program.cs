f1d7f8be1f5c29fee0cb47f5486b766e4907794b
Serge Konyakhin <sergekonyakhin@gmail.com>
2022-08-08 04:32:10 +0300

    Exercise№4 completed

---
+++ Exercise№4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите любое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int StartNumber = 2;
+
+while (StartNumber <= number)
+{
+    Console.Write(StartNumber + " ");
+    StartNumber = StartNumber + 2;
+}
